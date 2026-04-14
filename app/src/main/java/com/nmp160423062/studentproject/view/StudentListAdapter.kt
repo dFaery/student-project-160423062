8eac350543f977c34c138f1cb73090805d1c2f43
dFaery <137483979+dFaery@users.noreply.github.com>
2026-04-14 11:26:02 +0700

    Update StudentDetailFragment to fetch student data by ID using Navigation arguments
    - Update `main_navigation.xml` to define a string argument `nrp` for `studentDetailFragment`.
    - Update `StudentListAdapter` to pass the student's ID as the `nrp` argument when navigating to the detail fragment.
    - Update `StudentDetailFragment` to retrieve the `nrp` argument from the navigation bundle and pass it to the `viewModel.fetch()` method.

--- app/src/main/java/com/nmp160423062/studentproject/view/StudentListAdapter.kt
+++ app/src/main/java/com/nmp160423062/studentproject/view/StudentListAdapter.kt
@@ -27,7 +27,8 @@ class StudentListAdapter(val studentList:ArrayList<Student>)
         holder.binding.txtName.text = studentList[position].name
 
         holder.binding.btnDetail.setOnClickListener {
-            val action = StudentListFragmentDirections.actionDetailFragment()
+            val nrp = studentList[position].id
+            val action = StudentListFragmentDirections.actionDetailFragment(nrp.toString())
             Navigation.findNavController(it).navigate(action)
         }
 
